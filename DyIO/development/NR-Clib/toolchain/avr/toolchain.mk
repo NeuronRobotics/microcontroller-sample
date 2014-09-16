a76a7154635eb7ba555c46df7069f209dee0597b
madhephaestus <mad.hephaestus@gmail.com>
2014-09-16 13:02:51 -0400

    Removing other projects files

--- DyIO/development/NR-Clib/toolchain/avr/toolchain.mk
+++
@@ -1,5 +0,0 @@
-
-#Unix systems
-GCCA=avr-gcc -Wall -Os -fpack-struct -fshort-enums -funsigned-char -funsigned-bitfields
-ARAVR= avr-ar
-BIN2HEXAVR=avr-objcopy
--- DyIO/development/pic/src/Namespace_bcs_safe/Abstract_bsc_safe.c
+++
@@ -1,9 +0,0 @@
-/*
- * Abstract_bsc_safe.c
- *
- *  Created on: Mar 21, 2013
- *      Author: hephaestus
- */
-
-
-#include "Bowler/Bowler.h"
